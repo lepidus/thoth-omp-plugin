4c0f61e9521f6e4505e3f05598f177c4ed5b36ca
Thiago Matos <thiago@lepidus.com.br>
2026-08-12 18:15:02 -0400

    refactor: define external boundary contracts

---
+++ classes/Contracts/PluginLogger.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace APP\plugins\generic\thoth\classes\Contracts;
+
+interface PluginLogger
+{
+    public function info(string $message, array $context = []): void;
+
+    public function warning(string $message, array $context = []): void;
+
+    public function error(string $message, array $context = []): void;
+}
