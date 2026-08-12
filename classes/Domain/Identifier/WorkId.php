f902b6a5362dbe707c045c99011988f0e216d452
Thiago Matos <thiago@lepidus.com.br>
2026-08-12 16:17:01 -0400

    refactor: introduce Thoth domain identifiers

---
+++ classes/Domain/Identifier/WorkId.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace APP\plugins\generic\thoth\classes\Domain\Identifier;
+
+use InvalidArgumentException;
+
+final class WorkId
+{
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        if (!preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i', $value)) {
+            throw new InvalidArgumentException('Work ID must be a UUID');
+        }
+
+        $this->value = strtolower($value);
+    }
+
+    public function toString(): string
+    {
+        return $this->value;
+    }
+
+    public function equals(self $other): bool
+    {
+        return $this->value === $other->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
