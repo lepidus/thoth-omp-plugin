ec81719e79286eeb6f523b0887eeb1751ebb60fb
Thiago Matos <thiago@lepidus.com.br>
2026-08-12 16:17:01 -0400

    refactor: introduce Thoth domain identifiers

---
+++ classes/Domain/Identifier/PublicationId.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace APP\plugins\generic\thoth\classes\Domain\Identifier;
+
+use InvalidArgumentException;
+
+final class PublicationId
+{
+    private int $value;
+
+    public function __construct(int $value)
+    {
+        if ($value <= 0) {
+            throw new InvalidArgumentException('Publication ID must be positive');
+        }
+
+        $this->value = $value;
+    }
+
+    public function toInt(): int
+    {
+        return $this->value;
+    }
+
+    public function equals(self $other): bool
+    {
+        return $this->value === $other->value;
+    }
+}
