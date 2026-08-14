75903c236cd6e273f63f072b35291f7a663c0620
Thiago Matos <thiago@lepidus.com.br>
2026-08-13 20:31:08 -0400

    refactor: adapt legacy synchronizers to pipeline

---
+++ classes/Infrastructure/Legacy/LegacySubjectSynchronizer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace APP\plugins\generic\thoth\classes\Infrastructure\Legacy;
+
+use APP\plugins\generic\thoth\classes\Contracts\DomainSynchronizer;
+use APP\plugins\generic\thoth\classes\Domain\Identifier\WorkId;
+use APP\plugins\generic\thoth\classes\Domain\Result\SynchronizationResult;
+
+final class LegacySubjectSynchronizer implements DomainSynchronizer
+{
+    private object $service;
+
+    public function __construct(object $service)
+    {
+        $this->service = $service;
+    }
+
+    public function synchronize(object $desiredState, WorkId $workId): SynchronizationResult
+    {
+        $this->service->synchronizeByPublication($desiredState, $workId->toString());
+
+        return new SynchronizationResult();
+    }
+}
