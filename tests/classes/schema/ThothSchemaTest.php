a86a8629a13233a4c4c299a4210732ba5f9b11e2
Thiago Brasil <thiago@lepidus.com.br>
2026-08-20 18:54:49 -0400

    test: support PHPUnit 9 schema assertions

--- tests/classes/schema/ThothSchemaTest.php
+++ tests/classes/schema/ThothSchemaTest.php
@@ -43,7 +43,7 @@ class ThothSchemaTest extends PKPTestCase
         (new ThothSchema())->addToPublicationSchema('Schema::get::publication', $args);
 
         foreach (['Id', 'Title', 'Url', 'Width', 'Height', 'Sha256'] as $suffix) {
-            $this->assertObjectNotHasProperty('thothFeatureVideo' . $suffix, $schema->properties);
+            $this->assertFalse(property_exists($schema->properties, 'thothFeatureVideo' . $suffix));
         }
     }
 }
